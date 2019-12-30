baa5ed30f42ec8dda5e93797621690bce40aebb9
OsamaNadeem <osama.nadeem975@gmail.com>
2019-12-31 01:17:58 +0500

    Updates

---
+++ precache-manifest.8a829caf5222f65d216a0e4f37af0768.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8fc1af6397cee3ff3f55e5825c65027",
+    "url": "/artist_2/index.html"
+  },
+  {
+    "revision": "95aaa98f47466a05630b",
+    "url": "/artist_2/static/css/main.58ddea48.chunk.css"
+  },
+  {
+    "revision": "d99fdc6e46c7f8957f62",
+    "url": "/artist_2/static/js/2.fc4572e6.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/artist_2/static/js/2.fc4572e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95aaa98f47466a05630b",
+    "url": "/artist_2/static/js/main.b52f6074.chunk.js"
+  },
+  {
+    "revision": "bc3565ce437dff2ddc4d",
+    "url": "/artist_2/static/js/runtime-main.c9d40d1b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/artist_2/static/media/logo.5d5d9eef.svg"
+  }
+]);
